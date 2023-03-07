b48ce96e6401fa4851fe98760c9edd86e86ae9b8
Anton Nemtsev <thesilentimp@gmail.com>
2023-03-07 15:35:25 +0100

    Actions Scaffolder

---
+++ task-2.js
@@ -0,0 +1,24 @@
+/**
+ * Задача на розворот числа:
+ * @param {Number} currentMaxValue — число
+ * @returns {Number} перевернене число
+ * @example
+ * task(4589);
+ * // returns 9854
+ * @example
+ * task(1234);
+ * // returns 4321
+ * @example
+ * task(111);
+ * // returns 111
+ */
+
+const task = (currentMaxValue) => {
+  let reverseMaxValue;
+
+  // тут ваш код ...
+
+  return reverseMaxValue;
+};
+
+module.exports = task;
---
+++ task-3.js
@@ -0,0 +1,21 @@
+/**
+ * Знайдіть добуток масиву чисел з невідомою глибиною вкладеності:
+ * @param {Number[]|Number[][]} resultsArray — число
+ * @returns {Number} Добуток всіх елементрів масиву
+ * @example
+ * task([1, 2, [3, [4]]]);
+ * // returns 24
+ * @example
+ * task([2, 2]);
+ * // returns 4
+ */
+
+const task = (resultsArray) => {
+  let productOfArray;
+
+  // тут ваш код ...
+
+  return productOfArray;
+};
+
+module.exports = task;
